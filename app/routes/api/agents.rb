e31e2a80240384b5aae01cc2164aa076c6f7520b
robert <robert@r.uby.dev>
2026-08-28 03:14:46 +0000

    api: add create_agent! method

--- app/routes/api/agents.rb
+++ app/routes/api/agents.rb
@@ -5,22 +5,25 @@ class Raven::Routes::API
     route do |r|
       r.post(true) do
         check_csrf!
-        if agent = Raven::Agents::Robert.find_by(id: agent_id)
-          {ok: true, id: agent.id}
-        else
-          agent = Raven::Agents::Robert.create!
-          session["agent_id"] = agent.id
-          {ok: true, id: agent.id}
-        end
+        create_agent!(session["agent_id"])
       end
     end
 
     private
 
     ##
-    # @return [String]
-    def agent_id
-      session["agent_id"]
+    # Create an agent, yo.
+    # @param [String, nil] id
+    #  An existing agent id, if any
+    # @return [Hash]
+    def create_agent!(id)
+      if agent = Raven::Agents::Robert.find_by(id:)
+        {ok: true, id: agent.id}
+      else
+        agent = Raven::Agents::Robert.create!
+        session["agent_id"] = agent.id
+        {ok: true, id: agent.id}
+      end
     end
   end
 end
