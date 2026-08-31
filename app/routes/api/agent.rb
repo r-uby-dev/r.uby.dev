7ab69200c02aa3cd57e77477a659944620673a42
robert <robert@r.uby.dev>
2026-08-31 01:38:48 +0000

    agent: add 'talk' method

--- app/routes/api/agent.rb
+++ app/routes/api/agent.rb
@@ -5,21 +5,13 @@ class Raven::Routes::API
     route do |r|
       r.get(true) do
         r.sse do |sse|
-          q = r.params["q"].to_s
-          stream = Raven::Agents::Robert::Stream.new(sse).tap(&:hello)
-          agent = Raven::Agents::Robert.find(agent_id)
-          res = agent.talk(q, stream:)
-          stream.done(res:)
-        rescue ActiveRecord::RecordNotFound
-          stream.error(message: "agent unavailable")
-        rescue => e
-          stream.error(message: "internal server error (#{e.class})")
+          talk(r, sse)
         end
       end
 
       r.delete(true) do
         check_csrf!
-        Raven::Agents::Robert.find_by(id: agent_id)&.destroy
+        Robert.find_by(id: agent_id)&.destroy
         session.delete("agent_id")
         {ok: true}
       end
@@ -30,5 +22,21 @@ class Raven::Routes::API
     def agent_id
       session["agent_id"]
     end
+
+    ##
+    # Talk to an agent
+    # @return [void]
+    def talk(r, sse)
+      q      = r.params["q"].to_s
+      stream = Robert::Stream.new(sse).tap(&:hello)
+      agent  = Robert.find(agent_id)
+      res    = agent.talk(q, stream:)
+      stream.done(res:)
+    rescue ActiveRecord::RecordNotFound
+      stream.error(message: "agent unavailable")
+    rescue
+      stream.error(message: "internal server error")
+    end
+    include Raven::Agents
   end
 end
